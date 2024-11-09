ef463f9dd28f608edf1058066f7fb8566344e919
Juan Felipe Guarin Yusty <jguariny@unal.edu.co>
2024-11-08 20:21:10 -0500

    Segundo sufrimiento

--- homework/word_count.py
+++ homework/word_count.py
@@ -6,7 +6,7 @@ import fileinput
 import glob
 import os.path
 from itertools import groupby
-
+import string
 
 #
 # Escriba la función load_input que recive como parámetro un folder y retorna
@@ -22,10 +22,20 @@ from itertools import groupby
 #     ...
 #     ('text2.txt'. 'hypotheses.')
 #   ]
-#
+
+#for file in files:
+#        with open(file, "r") as f:
+#            for line in f:
+#                # proceso
+
 def load_input(input_directory):
     """Funcion load_input"""
-
+    files = glob.glob(f"{input_directory}/*")
+    sequence = []
+    with fileinput.input(files = files) as f:
+        for line in f:
+            sequence.append((fileinput.filename(), line))
+    return sequence
 
 #
 # Escriba la función line_preprocessing que recibe una lista de tuplas de la
@@ -34,6 +44,30 @@ def load_input(input_directory):
 #
 def line_preprocessing(sequence):
     """Line Preprocessing"""
+
+    # sequence = []
+    # for key, value in sequence:
+    #     new_tuple = (key, value.translate(str.maketrans("", "", string.punctuation)))
+    #     sequence.append(new_tuple)
+    #
+
+    sequence = [
+        (key, value.translate(str.maketrans("", "", string.punctuation)))
+        for key, value in sequence
+    ]
+
+    sequence = [
+        (key, value.lower())
+        for key, value in sequence
+    ]
+
+    sequence = [
+        (key, value.strip())
+        for key, value in sequence
+    ]
+    return sequence
+
+
 
 
 #
@@ -50,6 +84,7 @@ def line_preprocessing(sequence):
 #
 def mapper(sequence):
     """Mapper"""
+    return[(word, 1) for _, text_line in sequence for word in text_line.split()]
 
 
 #
@@ -65,6 +100,7 @@ def mapper(sequence):
 #
 def shuffle_and_sort(sequence):
     """Shuffle and Sort"""
+    return sorted(sequence, key=lambda x: x[0])
 
 
 #
@@ -75,6 +111,13 @@ def shuffle_and_sort(sequence):
 #
 def reducer(sequence):
     """Reducer"""
+    diccionario = {}
+    for key, value in sequence:
+        if key in diccionario:
+            diccionario[key] += value
+        else:
+            diccionario[key] = value
+    return list(diccionario.items())
 
 
 #
@@ -83,6 +126,11 @@ def reducer(sequence):
 #
 def create_ouptput_directory(output_directory):
     """Create Output Directory"""
+    if os.path.exists(output_directory):
+        for file in glob.glob(f"{output_directory}/*"):
+            os.remove(file)
+        os.rmdir(output_directory)
+    os.makedirs(output_directory)
 
 
 #
@@ -95,6 +143,9 @@ def create_ouptput_directory(output_directory):
 #
 def save_output(output_directory, sequence):
     """Save Output"""
+    with open(f"{output_directory}/part-00000", "w", encoding="utf-8") as f:
+        for key, value in sequence:
+            f.write(f"{key}\t{value}\n")
 
 
 #
@@ -103,6 +154,8 @@ def save_output(output_directory, sequence):
 #
 def create_marker(output_directory):
     """Create Marker"""
+    with open(f"{output_directory}/_SUCCESS", "w", encoding="utf-8") as f:
+        f.write("")
 
 
 #
@@ -110,10 +163,24 @@ def create_marker(output_directory):
 #
 def run_job(input_directory, output_directory):
     """Job"""
+    sequence = load_input(input_directory)
+    sequence = line_preprocessing(sequence)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
+    
+    from pprint import pprint
+    print()
+    pprint(sequence)
+    print()
 
 
 if __name__ == "__main__":
     run_job(
-        "input",
-        "output",
+        "files/input",
+        "files/output",
     )
